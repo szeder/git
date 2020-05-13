e0f46c2a7eb5b47966b4344e548b5db3e794fffa
SZEDER Gábor <szeder.dev@gmail.com>
2020-05-13 08:46:01 +0200

    TEST: run ./t5500-fetch-pack.sh --stress -r 44 with tweaks #3

--- ci/run-build-and-tests.sh
+++ ci/run-build-and-tests.sh
@@ -14,7 +14,8 @@ make -k
 
 cd t
 
-export GIT_TEST_COMMIT_GRAPH=1
-export GIT_TEST_MULTI_PACK_INDEX=1
+export GIT_TEST_FULL_IN_PACK_ARRAY=true
+export GIT_TEST_OE_SIZE=10
+export GIT_TEST_OE_DELTA_SIZE=5
 ./t5500-fetch-pack.sh --stress -r 44
 
