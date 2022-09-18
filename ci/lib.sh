2362d34f8217369eaab98dade89ba258401e2ed3
SZEDER Gábor <szeder.dev@gmail.com>
2022-09-18 13:07:56 +0200

    ci: check that 'make install' doesn't build anything
    [1] d3b827408c (Makefile: build 'gitweb' in the default target,
                    2022-06-28)
        2530afd351 (Makefile: generate Git(3pm) as dependency of the 'doc'
                    and 'man' targets, 2018-02-15)
    
    Signed-off-by: SZEDER Gábor <szeder.dev@gmail.com>

--- ci/lib.sh
+++ ci/lib.sh
@@ -177,6 +177,21 @@ check_unignored_build_artifacts () {
 	}
 }
 
+check_install_did_not_build_anything () {
+	local stamp=/tmp/before-install-stamp changelist=/tmp/changed-files
+
+	>"$stamp"
+	"$@"
+
+	find . -newer "$stamp" \! -name .git >"$changelist"
+	if test -s "$changelist"
+	then
+		cat "$changelist"
+		echo "$(tput setaf 1)error: '$@' created/modified some files$(tput sgr0)"
+		return 1
+	fi
+}
+
 check_uncleaned_build_artifacts () {
 	if git ls-files --ignored --other --exclude-standard --error-unmatch \
 		-- ':/*' 2>/dev/null
@@ -377,7 +392,7 @@ osx-meson)
 	;;
 esac
 
-MAKEFLAGS="$MAKEFLAGS CC=${CC:-cc}"
+MAKEFLAGS="$MAKEFLAGS CC=${CC:-cc} prefix=/tmp/git-install"
 
 end_group "CI setup via $(basename $0)"
 set -x
